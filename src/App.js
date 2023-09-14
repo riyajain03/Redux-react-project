de113fa84ce73f3e3a0bb0661176069c8b85a130
= <=>
2023-09-14 15:33:12 +0530

    Implimented redux

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" exact component={ProductDetail} />
+          <Route>404 not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/actions/productActions.js
@@ -0,0 +1,15 @@
+import { ActionTypes } from "../constants/action-types"
+
+export const setProducts = (Products) => {
+    return {
+        type:ActionTypes.SET_PRODUCTS,
+        payload: Products,
+    }
+}
+
+export const selectedProduct= (Product) => {
+    return {
+        type:ActionTypes.SELECTED_PRODUCT,
+        payload: Product,
+    }
+}
